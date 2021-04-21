dc71b87f3ba040e273d33b771544bbdf5fa68872
infer <infer@LAPTOP-0LDL4DMA>
2021-04-21 21:25:12 +0200

    Cambios en las clases (en el excel de la práctica 4 aparece un cambio, pero es porque entre a hacer copia y pega de la columna n para introducirla en la práctica 6, no hay realmente ningún cambio ni toque nada más)

--- src/main/java/algestudiante/p6/MejorListaTiempos.java
+++ src/main/java/algestudiante/p6/MejorListaTiempos.java
@@ -2,8 +2,29 @@ package algestudiante.p6;
 
 public class MejorListaTiempos {
 	
+	static MejorLista mj;
+	
 	public static void main(String[] args) {
-		System.out.print("Hola");
+		//MejorLista mj = new MejorLista("Lista01",100);
+		//mj.backtracking(0);
+		//mj.print();
 		
+		long t1, t2;
+		int nVeces = 100;
+		
+		for (int n = 100; n < 100000000; n*=2) {
+			mj = new MejorLista("Lista01",n);
+
+			t1 = System.currentTimeMillis();
+			
+			for(int rep = 0; rep < nVeces; rep++) {
+				mj.backtracking(0);
+			}
+
+			t2 = System.currentTimeMillis();
+
+			long tMedio = t2 - t1;
+			System.out.println(n + "\t" + tMedio);
+		}
 	}
 }
