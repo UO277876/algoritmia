0606c95be141feed01cb92dd954d668fa59b8b0d
infer <infer@LAPTOP-0LDL4DMA>
2021-04-21 21:11:56 +0200

    Nuevo intento de clase de medición de tiempos

--- src/main/java/algestudiante/p6/MejorListaTiempos.java
+++ src/main/java/algestudiante/p6/MejorListaTiempos.java
@@ -1,11 +1,9 @@
 package algestudiante.p6;
 
 public class MejorListaTiempos {
-
-	static MejorLista mj;
 	
 	public static void main(String[] args) {
-		mj = new MejorLista("Lista01",100);
+		System.out.print("Hola");
 		
 	}
 }
