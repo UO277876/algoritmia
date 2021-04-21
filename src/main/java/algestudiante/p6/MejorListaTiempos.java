ca4049dd0daa797b0bbef4368b7deebc76832244
infer <infer@LAPTOP-0LDL4DMA>
2021-04-21 21:06:20 +0200

    ¿Arreglo de la clase?

---
+++ src/main/java/algestudiante/p6/MejorListaTiempos.java
@@ -0,0 +1,11 @@
+package algestudiante.p6;
+
+public class MejorListaTiempos {
+
+	static MejorLista mj;
+	
+	public static void main(String[] args) {
+		mj = new MejorLista("Lista01",100);
+		
+	}
+}
