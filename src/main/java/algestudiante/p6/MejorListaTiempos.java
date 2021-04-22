34dd99d242ecd9c01d4a062c33e680b908733def
infer <infer@LAPTOP-0LDL4DMA>
2021-04-22 15:57:53 +0200

    Corrección main

--- src/main/java/algestudiante/p6/MejorListaTiempos.java
+++ src/main/java/algestudiante/p6/MejorListaTiempos.java
@@ -2,23 +2,27 @@ package algestudiante.p6;
 
 public class MejorListaTiempos {
 	
-	static MejorLista mj;
+	static MejorLista mjt,mj;
 	
 	public static void main(String[] args) {
-		//MejorLista mj = new MejorLista("Lista01",100);
-		//mj.backtracking(0);
-		//mj.print();
+		// Corrección
+		// NOTA: Del fichero SOLO el nombre, no la terminación ".txt"
+		String fichero = args[0];
+		int tam = Integer.valueOf(args[1]);
+		mj = new MejorLista(fichero,tam);
+		mj.backtracking(0);
+		mj.print();
 		
 		long t1, t2;
 		int nVeces = 1;
 		int index = 1;
 		for (int n = 5; n < 31; n*=index) {
-			mj = new MejorLista(n);
+			mjt = new MejorLista(n);
 
 			t1 = System.currentTimeMillis();
 			
 			for(int rep = 0; rep < nVeces; rep++) {
-				mj.backtracking(0);
+				mjt.backtracking(0);
 			}
 
 			t2 = System.currentTimeMillis();
