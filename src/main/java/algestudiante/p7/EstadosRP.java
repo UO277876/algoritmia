e4133123d0ac1afef0b0922b32ab5a9b55798e64
infer <infer@LAPTOP-0LDL4DMA>
2021-05-05 20:32:48 +0200

    Ramifica y poda versión 1

--- src/main/java/algestudiante/p7/EstadosRP.java
+++ src/main/java/algestudiante/p7/EstadosRP.java
@@ -4,49 +4,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import algestudiante.p7.util.Estado;
-import algestudiante.p7.util.RamificaYPoda;
 
 /**
  * Estado Asignaci�n Agentes - Tareas
  * Hereda de la clase Estado
  */
-public class EstadoAsigna extends Estado 
+public class EstadosRP extends Estado 
 {
 	// Datos comunes a todos los estados, por eso los declaramos "static"
-	private static int n;				// Tama�o del problema, n�m. agentes y n�m. tareas
-	private static int[][] costes;		// matriz costes
+	private static int tmax;				// Tiempo máximo
+	private static CancionRP[] canciones;		// Lista de canciones
 	private static boolean traza= true;	// permite activar o desactivar traza
 
 	// Estado
 	private int[] solParcial;				// solParcial[i]=k es al trabajador i se le asigna la tarea k 
 	private boolean[] tareaConTrabajador;	// Marca las tareas ya asignadas
-
+	
+	private int duracionActual;
+	private int puntuacionActual;
+	
 	/**
 	 * Constructor para crear el estado inicial,
 	 * Establecemos los datos y condiciones iniciales del problema
 	 */
-	public EstadoAsigna() {
+	public EstadosRP(CancionRP[] listaCanciones, int t) {
 		super();
 		
-		// Condiciones iniciales
-		n= 4;		// n�mero de agentes y tareas
-		// matriz de costes
-		costes = new int[n][n];
-		costes[0][0]=58;costes[0][1]=67;costes[0][2]=58;costes[0][3]=65;
-		costes[1][0]=60;costes[1][1]=68;costes[1][2]=65;costes[1][3]=55;
-		costes[2][0]=62;costes[2][1]=70;costes[2][2]=67;costes[2][3]=56;
-		costes[3][0]=56;costes[3][1]=68;costes[3][2]=60;costes[3][3]=54; 
-
-		/**/if (RamificaYPoda.TRAZA)
-			imprimirCostes();
+		// Guardamos el tiempo máximo y la lista de canciones
+		tmax = t*60;
+		canciones = listaCanciones;
 
 		// Inicializa variables de cada estado
-		solParcial = new int[n];
+		solParcial = new int[canciones.length];
 		for (int i=0;i<solParcial.length;i++)
 			solParcial[i] = -1;//sin asignaciones inicialmente
 		
 		tareaConTrabajador=new boolean[solParcial.length];
-		for (int i= 0; i<n; i++)
+		for (int i= 0; i<canciones.length; i++)
 			tareaConTrabajador[i]= false;
 		
 	}
@@ -54,11 +48,11 @@ public class EstadoAsigna extends Estado
 	/**
 	 * Constructor para crear un estado a partir del padre
 	 */
-	public EstadoAsigna(EstadoAsigna padre,int j) {
+	public EstadosRP(EstadosRP padre,int j) {
 		super();
 		// copiamos toda la informaci�n del padre		
-		solParcial= Arrays.copyOf(padre.solParcial,n);
-		tareaConTrabajador= Arrays.copyOf(padre.tareaConTrabajador,n);
+		solParcial= Arrays.copyOf(padre.solParcial,canciones.length);
+		tareaConTrabajador= Arrays.copyOf(padre.tareaConTrabajador,canciones.length);
 		profundidad= padre.profundidad;
 		idPadre= padre.getId();
 		
@@ -69,11 +63,11 @@ public class EstadoAsigna extends Estado
 		calcularValorHeuristico();
 		
 		// UNIFICA LOS DOS �LTIMOS NIVELES DEL �RBOL DE ESTADOS
-		if (profundidad==n-1)
+		if (profundidad==canciones.length-1)
 		{
 			int tareaPorAsignar= -1;
 			// Busca �ltima tarea por asignar
-			for (int k= 0; k<n; k++)
+			for (int k= 0; k<canciones.length; k++)
 				if (!tareaConTrabajador[k])
 					tareaPorAsignar= k;
 			
@@ -90,49 +84,32 @@ public class EstadoAsigna extends Estado
 	 */
 	@Override
 	public void calcularValorHeuristico() {
-		valorHeuristico = 0;
+		if(solucion()) {
+			valorHeuristico = puntuacionActual;
+		} else {
+			double mejor = (canciones[profundidad+1].getPuntuacion()/canciones[profundidad+1].getSegundos())+0.5;
+			duracionActual += canciones[profundidad+1].getSegundos();
+			puntuacionActual += canciones[profundidad+1].getPuntuacion();
+			valorHeuristico = (int) (canciones[profundidad].getPuntuacion()+(duracionActual - canciones[profundidad].getSegundos())*mejor);
+		}
 		
-		// Suma los costes de los agentes con tarea
-		for (int i= 0; i<profundidad; i++)
-			valorHeuristico+= costes[i][solParcial[i]];
-		
-		// Suma los m�nimos de lo que resta de columna por cada tarea no asignada
-		for (int j = 0; j<n; j++)
-			if (!tareaConTrabajador[j])
-				valorHeuristico+= minimoColumna(profundidad,j);
-		
-	}
-
-	/**
-	 * Calcula el valor m�nimo por columnas
-	 * @param profundidad	para saber cuantas tareas est�n ya asignadas
-	 * @param j				para saber que tarea se asigna en este paso
-	 * @return				valor m�nimo por columnas
-	 */
-	private int minimoColumna(int profundidad, int j) {
-		int minValorColumna=Integer.MAX_VALUE;
-		for (int i= profundidad; i<n; i++)
-			// calculamos el m�nimo de columnas
-			if (costes[i][j] < minValorColumna)
-				minValorColumna= costes[i][j];
-		return minValorColumna;
 	}
 
 	@Override
 	public ArrayList<Estado> expandir() {
-		ArrayList<Estado> listaHijos= new ArrayList<Estado>();
+		ArrayList<Estado> listaHijos = new ArrayList<Estado>();
 
 		// Recorre todas las tareas
-		for (int j=0; j<n; j++)
-			if (!tareaConTrabajador[j])  // Crea hijo s�lo para tareas NO asignadas
-			{
-				Estado estadoHijo= new EstadoAsigna(this,j); 
-				listaHijos.add(estadoHijo);
-				
-				if (traza)  // imprimimos estado hijo generado
-				{
-					System.out.println("Nivel: "+profundidad+" - Hijo: "+j);
-					System.out.println(estadoHijo);
+		for (int i = 0; i <= 2; i++)
+			if (duracionActual <= tmax) {
+				if (i == 1) {
+					Estado estadoHijo1 = new EstadosRP(this, i);
+					listaHijos.add(estadoHijo1);
+				}
+
+				if (i == 2) {
+					Estado estadoHijo2 = new EstadosRP(this, i);
+					listaHijos.add(estadoHijo2);
 				}
 			}
 
@@ -141,53 +118,10 @@ public class EstadoAsigna extends Estado
 
 	@Override
 	public boolean solucion() {
-		if (profundidad==n)
+		if (profundidad == canciones.length)
 			return true;
 		else
 			return false;
-	}
-	
-	@Override
-	public int valorInicialPoda() 
-	/* Inicialmente el valor de poda va a ser la menor de la 
-   suma de ambas diagonales */
-	{
-		int diagonal1= 0;
-		for (int i=0;i<n;i++)
-			diagonal1 += costes[i][i];
-		int diagonal2= 0;
-		for (int i=0;i<n;i++)
-			diagonal2 += costes[i][costes.length-1-i];
-
-		/**/if (RamificaYPoda.TRAZA)	// muestra el valor inicial
-		{
-			System.out.println("Valor inicial de poda");
-			System.out.println("Diagonal 1: "+diagonal1);
-			System.out.println("Diagonal 2: "+diagonal2);
-			System.out.println("Valor cota de poda: "+(diagonal1<=diagonal2?diagonal1:diagonal2)+"\n");
-		}
-		
-		if (diagonal1<=diagonal2) 
-			return diagonal1;
-		else 
-			return diagonal2;
-	}
-
-	/**
-	 * Imprime la matriz de costes
-	 */
-	public void imprimirCostes()
-	{
-		System.out.println("N�mero de agentes / tareas = "+n);
-		System.out.println("Matriz de costes");
-		for (int i=0; i<n; i++)
-		{
-			for (int j=0; j<n; j++)
-				System.out.printf("%5d", costes[i][j]);
-			System.out.println();
-		}
-		System.out.println();
-
 	}
 
 	@Override
--- src/main/java/algestudiante/p7/MejorListaRP.java
+++ src/main/java/algestudiante/p7/MejorListaRP.java
@@ -3,28 +3,39 @@
 
 package algestudiante.p7;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 import algestudiante.p7.util.RamificaYPoda;
 
 /**
- * Clase principal para el problema de Asignaci�n de agentes - tareas
+ * Clase principal para el problema de Asignacion de agentes - tareas
  * Herada de RamificaYPoda 
  */
-class Asignacion extends RamificaYPoda 
+class MejorListaRP extends RamificaYPoda 
 {	
 	private static boolean traza= false;
+	private static int tam = 0;
+	private static CancionRP[] canciones;
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Problema de Agentes - tareas /// Ramificaci�n y poda");
+		System.out.println("Problema de Agentes - tareas /// Ramificacion y poda");
+		// EL NOMBRE DEL FICHERO DEBE IR SIN LA TERMINACIÓN .TXT
+		String nameFile = args[0];
+		tam = Integer.valueOf(args[1]);
 		
 		// Creamos una instancia para resolver el problema
-		Asignacion problemaAsigna = new Asignacion(); 
+		MejorListaRP problemaAsigna = new MejorListaRP(nameFile); 
 
 		// Ejecutamos el m�todo que va recorriendo el espacio de soluciones con ramifica y poda
 		problemaAsigna.ramificaYPoda(problemaAsigna.getNodoRaiz()); 
 		
 		// Soluci�n final
-		System.out.println("Soluci�n �ptima del problema $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("Solucion optima del problema $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
 		System.out.println(problemaAsigna.mejorSolucion);
 
 		if (traza)
@@ -35,9 +46,76 @@ class Asignacion extends RamificaYPoda
 	/**
 	 * Constructor crea el estaod inicial para iniciar el problema
 	 */
-	public Asignacion() 
+	public MejorListaRP(String nameFile) 
 	{
-		nodoRaiz = new EstadoAsigna(); //costes iniciales
+		readDatosFromFile(nameFile);
+		nodoRaiz = new EstadosRP(canciones,tam); //costes iniciales
+	}
+	
+	/**
+	 * Lee de fichero y transforma los datos en una lista de enteros
+	 * 
+	 * @param file, el fichero de texto
+	 * @return Una lista de enteros
+	 */
+	public void readDatosFromFile(String file) {
+		BufferedReader fich = null;
+		String line;
+		int pos = 0;
+		int index = 0;
+		String fileName = Paths.get("").toAbsolutePath().toString() + "/src/main/java/algestudiante/p6/datos/" + file
+				+ ".txt";
+
+		try {
+			// abrimos el fichero
+			fich = new BufferedReader(new FileReader(fileName));
+			line = fich.readLine(); // Primer elemento del fichero
+			while (line != null) {
+				if (pos == 0) {
+					int nCanciones = Integer.valueOf(line);
+					canciones = new CancionRP[nCanciones];
+					line = fich.readLine();
+					pos++;
+				} else {
+					String[] parts = line.split("\t");
+					introducirCancion(parts, index);
+					index++;
+					line = fich.readLine();
+					pos++;
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No existe el fichero: " + fileName);
+		} catch (IOException e) {
+			System.out.println("Error leyendo el fichero: " + fileName);
+		} finally {
+			if (fich != null)
+				try {
+					fich.close();
+				} catch (IOException e) {
+					System.out.println("Error cerrando el fichero: " + fileName);
+					e.printStackTrace();
+				}
+		}
+	}
+
+	/**
+	 * Crea la nueva canción para la lista de canciones
+	 * 
+	 * @param parts, las partes de cada canción
+	 * @param index, la fila donde va en el vector
+	 */
+	private void introducirCancion(String[] parts, int index) {
+		String name = parts[0];
+		int puntuacion = Integer.valueOf(parts[2]);
+
+		String[] time = parts[1].split(":");
+		int min = Integer.valueOf(time[0]);
+		int seg = Integer.valueOf(time[1]);
+
+		seg += min * 60;
+
+		canciones[index] = new CancionRP(seg, name, puntuacion);
 	}
 
 }
